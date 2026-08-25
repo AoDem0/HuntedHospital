6a2662678e4166778cfd0828826f7b2e1cc8ac4a
AoDemo <dsw52708@student.dsw.edu.pl>
2026-08-25 13:42:59 +0200

    deals panels & dealsSO & TMPro

---
+++ HuntedHospital/Assets/Scripts/DeathDealPanel.cs
@@ -0,0 +1,45 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathDealPanel : MonoBehaviour
+{
+    [SerializeField] private DeathDealSO deathDeal;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Button button;
+
+    private void Awake()
+    {
+        text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        button = GetComponent<Button>();
+        if(text!=null) Debug.Log("found text");
+        if(button!=null) Debug.Log("found button");
+    }
+
+    private void Start()
+    {//do zmiany, tym ma zarzadzac gameman
+        ReloadPanel(deathDeal);
+    }
+
+    public void ReloadPanel(DeathDealSO deal)
+    {
+        if (text == null || button == null) return;
+        button.interactable = true;
+        text.text = "";
+        for (int i = 0; i < deal.Deals.Count; i++)
+        {
+            text.text += deal.Deals[i].dealAmount.ToString() + " " + deal.Deals[i].dealType.ToString() + "\n";
+        }
+
+        text.text += "Price: " + deal.SoulPrice.ToString();
+    }
+
+    //public void 
+    public void DeactivateDeathPanel()
+    {
+        text.text = "";
+        button.interactable = false;
+
+    }
+}
---
+++ HuntedHospital/Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float BloodLevel = 0f;
+    public float SoulsLevel = 0f;
+    public float TimeOfDay;
+    public int Days;
+}
