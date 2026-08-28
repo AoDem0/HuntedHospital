71acd5ff8f46d9cfe962cd1aa165a184239c4bf1
AoDemo <dsw52708@student.dsw.edu.pl>
2026-08-28 23:23:48 +0200

    sound&soundman&settings

--- HuntedHospital/Assets/scripts/MenuButtons.cs
+++ HuntedHospital/Assets/scripts/MenuButtons.cs
@@ -1,14 +1,35 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    [SerializeField]private soundManager soundMan;
+    private void Start()
+    {
+        soundMan = FindAnyObjectByType<soundManager>();
+    }
+
     public void QuitGame()
     {
+        soundMan.Play("uiclick");
         Application.Quit();
     }
     public void GameStart()
     {   
+        soundMan.Play("uiclick");
         SceneManager.LoadScene("MainHospitalScene");
+        
+    }
+
+    public void BackButton()
+    {
+        soundMan.Play("uiclick");
+        SceneManager.LoadScene("Menu");
+    }
+    public void SettingsButton()
+    {
+        soundMan.Play("uiclick");
+        SceneManager.LoadScene("SoundSettings");
     }
 }
---
+++ HuntedHospital/Assets/scripts/soundManager.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string soundName;
+    public AudioClip clip;
+    [Range(0f, 1f)] 
+    public float volume;
+    [Range(0f, 1f)] 
+    public float pitch;
+
+    [HideInInspector] public AudioSource source;
+}
+public class soundManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+        }
+    }
+    
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.soundName == name);
+        s.source.Play();
+    }
+}
